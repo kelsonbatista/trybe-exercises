9b5e7cc6456e25a6c8dc9afe5d6068d42b1d64c3
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-08 18:15:24 -0300

    practical examples course 4.1

--- 01-fundamentals/04-javascript-n-logic/01-introduction-to-javascript/course41/js/aula.js
+++ 01-fundamentals/04-javascript-n-logic/01-introduction-to-javascript/course41/js/aula.js
@@ -1,5 +1,76 @@
-let age = 20;
-let firstName = "Kelson";
+//Parte 1
+//let age = 20;
+//let firstName = "Kelson";
+//console.log(age);
+//console.log(firstName);
 
-console.log(age);
-console.log(firstName);
+//Parte 2
+// let movie = "Avengers"; //tipo string literal - vc esta adotando uma string para variavel
+// let score = 10; // tipo number literal - estou atribuindo numero inteiro ou quebrados (float) a variavel
+// let isValid = true; // tipo booleano , pode ser 1/0 ou true/false
+// let firstName; // tipo indefinida , nao sabe se é string, numero, booleano, pois nao tem valor ainda
+// let color = null; //tipo nulo - usado por ex. em um site começa sem cor definida, e o usuario muda, confirmando
+
+// let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+
+// salary++;
+// console.log(salary);
+
+// Parte 3 - If Else
+// let trybe = 17.10;
+
+// if (trybe >= 14 && trybe < 14.40 ) {
+//   console.log("Horário de esquenta");
+// } else if (trybe >= 16.30 && trybe < 17.50) {
+//   console.log("Aula ao vivo");
+// } else if (trybe >=19.40 && trybe < 20) {
+//   console.log("Fechamento");
+// } else {
+//   console.log("Fora dos momentos síncronos");
+// }
+
+//Parte 4 - Operadores logicos
+// const comida = 'pao na chapa';
+// const bebida = 'cafezinho';
+
+// if (bebida === 'cafezinho' &&  comida === 'pao na chapa') {
+//   console.log("Muito obrigado pela refeição!");
+// } else {
+//   console.log("Acho que houve engano com meu pedido.");
+// }
+
+//Continua
+// const condicao1 = true;
+// const condicao2 = false;
+
+// console.log(condicao1 || condicao2);
+
+//Switch /Case
+
+let trafficLight = "roxo";
+
+switch (trafficLight) {
+  case "vermelho": 
+    console.log("pare");
+    break;
+
+  case "amarelo":
+    console.log("atencao");
+    break;
+
+  case "verde":
+    console.log("siga");
+    break;
+
+  default:
+    console.log("Valor nao identificado");
+    break;
+}
+
+
--- 01-fundamentals/04-javascript-n-logic/01-introduction-to-javascript/course41/js/exercicio.js
+++ 01-fundamentals/04-javascript-n-logic/01-introduction-to-javascript/course41/js/exercicio.js
@@ -1,9 +1,90 @@
-let myName = "Kelson";
-let birthCity = "São José dos Campos";
-let birthYear = 1979;
-birthYear = 2030;
-birthCity = "São Paulo";
+//Parte 1
+// let myName = "Kelson";
+// let birthCity = "São José dos Campos";
+// let birthYear = 1979;
+// birthYear = 2030;
+// birthCity = "São Paulo";
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+//Parte 2
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientEmail);
+// console.log(typeof patientAge);
+// console.log(typeof patientId);
+
+//Continua
+// let base = 5;
+// let height = 8;
+// const area = base * height;
+// let perimeter = (base * 2) + (height * 2);
+
+// console.log(area + ' m2');
+// console.log(perimeter + ' m');
+
+// Parte 3 if else
+// const nota = 80.5;
+
+// if (nota >= 80) {
+//   console.log("Parabéns, você foi aprovado(a)!");
+// } else if (nota >= 60 && nota < 80) {
+//   console.log("Você está na nossa lista de espera");
+// } else {
+//   console.log("Você foi reprovado(a)!");
+// }
+
+// Parte 4 operadores logicos
+//Operador AND &&
+// const currentHour = 2;
+// var message = "";
+
+// if (currentHour >= 22) {
+//   message = "Hora de dormir";
+// } else if (currentHour >= 18 && currentHour < 22) {
+//   message = "Hora de jantar";
+// } else if (currentHour >= 14 && currentHour < 18) {
+//   message = "Hora do café da tarde";
+// } else if (currentHour >= 11 && currentHour < 14) {
+//   message = "Hora do almoço";
+// } else if (currentHour >= 6 && currentHour < 11) {
+//   message = "Hora do café da manhã";
+// } else {
+//   message = "Dormindo...";
+// }
+// console.log(message);
+
+//Operador OU ||
+// var weekDay = 'sabado';
+// if (weekDay == 'sabado' || weekDay == 'domingo') {
+//   console.log("FINALMENTE!!!")
+// } else {
+//   console.log("Mais um dia de aprendizado");
+// }
+
+
+// Switch case
+let resultado = 'aprovado';
+
+switch (resultado) {
+  case ('aprovado'):
+    console.log("Você foi aprovado(a)!");
+    break;
+  case ('lista'):
+    console.log("Você entrou na lista de espera");
+    break;
+  case ('reprovado') :
+    console.log("Você foi reprovado(a)!");
+    break;
+  default:
+    console.log("Procure a secretaria");
+    break;
+}
