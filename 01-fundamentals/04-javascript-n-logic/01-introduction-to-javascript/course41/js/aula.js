9847756ad164df64ec807b2720e63dae3602b8f5
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-08 12:43:21 -0300

    first exercise

---
+++ 01-fundamentals/04-javascript-n-logic/01-introduction-to-javascript/course41/js/aula.js
@@ -0,0 +1,5 @@
+let age = 20;
+let firstName = "Kelson";
+
+console.log(age);
+console.log(firstName);
---
+++ 01-fundamentals/04-javascript-n-logic/01-introduction-to-javascript/course41/js/exercicio.js
@@ -0,0 +1,9 @@
+let myName = "Kelson";
+let birthCity = "São José dos Campos";
+let birthYear = 1979;
+birthYear = 2030;
+birthCity = "São Paulo";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
