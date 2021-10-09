48bfda2f9fc82fc1337f9f883dd151d462590430
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-09 20:22:10 -0300

    ex42 main item9

---
+++ 01-fundamentals/04-javascript-n-logic/02-javascript-array-n-loop-for/exercises42/js/main-item9.js
@@ -0,0 +1,13 @@
+
+// Exercicio principal do bloco
+
+//ITEM 9
+// criar array de 1 a 25 e imprimir o resultado dos arrays dividido por 2
+
+let numbers = [];
+
+for (let i = 1; i <= 25; i += 1) {
+  let divided = i / 2;
+  numbers.push(divided);
+}
+console.log(numbers);
