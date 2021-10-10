7f1db42dc04263e115bbf84e54480e3d0611aca9
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-10 00:19:29 -0300

    ex43 practice1

---
+++ 01-fundamentals/04-javascript-n-logic/03-javascript-programming-logic-n-algorithms/exercises43/js/practice1.js
@@ -0,0 +1,14 @@
+
+// Item 1
+// dado um n (>1) imprimir um quadrado de asteriscos de lado tamanho n
+
+let n = 5;
+let asterisk = '*'
+
+for (let i = 0; i < n; i += 1) {
+  console.log(asterisk.repeat(n));
+}
+
+
+// Ref. repetir um caracter por n vezes
+// https://stackoverflow.com/questions/1877475/repeat-a-string-in-javascript-a-number-of-times
