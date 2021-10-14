86a5cd448d96c6cfb260779d0fcf43e8e5a7885f
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-13 22:45:31 -0300

    exercise megasena numbers

--- 01-fundamentals/04-javascript-n-logic/02-javascript-array-n-loop-for/lectures42/js/code1.js
+++ 01-fundamentals/04-javascript-n-logic/02-javascript-array-n-loop-for/lectures42/js/code1.js
@@ -2,23 +2,27 @@
 
 
 let megaSenaNumbers = [];
-let myNumbers = [10,22,09,08,07,02];
+let myNumbers = [10,22,27,35,42,47];
+let acertos = 0;
 
 for (let i=0; i <= 5; i++) {
   let n = Math.ceil(Math.random() * 60);
 	megaSenaNumbers.push(n);
 }
 
-for (let i=0; i <= megaSenaNumbers.length; i++) {
-
-  for (let j=0; j <= megaSenaNumbers.length; j++) {
-
+for (let i=0; i < megaSenaNumbers.length; i++) {
+  for (let j=0; j < megaSenaNumbers.length; j++) {
     if(myNumbers[i] == megaSenaNumbers[j]) {
-      console.log("Acertou: ", i)
-    } else {
-      console.log("Não acertou: ", i) 
+      acertos = 1;
     }
   }
+  if (acertos == 1) {
+    console.log("Acertou:", myNumbers[i]);
+    acerto = 0;
+  } else {
+    console.log("Errou:", myNumbers[i]);
+  }
+  acertos = 0;
 }
 
 console.log("Números da megasena: ", megaSenaNumbers);
