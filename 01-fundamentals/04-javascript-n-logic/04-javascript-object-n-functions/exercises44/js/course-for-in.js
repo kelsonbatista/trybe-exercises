29fe3413e851cd6c123faea45ada00c335583e64
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-15 22:09:18 -0300

    course for in examples

---
+++ 01-fundamentals/04-javascript-n-logic/04-javascript-object-n-functions/exercises44/js/course-for-in.js
@@ -0,0 +1,30 @@
+
+
+let food = ['hamburguer','pizza','esfiha'];
+
+let fruits = {
+  name: 'laranja',
+  price: '10.99',
+  unit: 'Kg'
+}
+
+// for in
+// for(let i in fruits) {
+//   console.log(i);
+// }
+
+// for(let i in food){
+//   console.log(i);
+// }
+
+
+// for of
+for(let i of fruits) {
+  console.log(fruits[i]);
+}
+
+// for(let i of food) {
+//   console.log(i);
+// }
+
+
