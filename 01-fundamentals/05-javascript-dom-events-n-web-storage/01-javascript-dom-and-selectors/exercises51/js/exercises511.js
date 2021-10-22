0c4150435b021aa6b9299ed925f4ccd8e2f2ed30
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-21 21:24:23 -0300

    exercises51 updated

--- 01-fundamentals/05-javascript-dom-events-n-web-storage/01-javascript-dom-and-selectors/exercises51/js/exercises511.js
+++ 01-fundamentals/05-javascript-dom-events-n-web-storage/01-javascript-dom-and-selectors/exercises51/js/exercises511.js
@@ -6,7 +6,14 @@ document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
 document.querySelector('.no-emergency-tasks').style.backgroundColor = 'lightyellow';
 
 
-let emergency = document.querySelector('.emergency-tasks');
-for (let i = 0; i < emergency.length; i += 1) {
-  emergency[i].style.backgroundColor = 'purple';
+let emg = document.querySelectorAll(".emergency-tasks h3");
+for (let i = 0; i < emg.length; i += 1) {
+  emg[i].style.backgroundColor = 'purple';
 }
+
+let noemg = document.querySelectorAll(".no-emergency-tasks h3");
+for (let i = 0; i < noemg.length; i += 1) {
+  noemg[i].style.backgroundColor = 'black';
+}
+
+document.querySelector('#footer-container').style.backgroundColor = 'darkgreen';
