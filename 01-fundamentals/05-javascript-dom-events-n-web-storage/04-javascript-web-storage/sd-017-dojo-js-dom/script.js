99ecc6753a8d8f3f9bde3f1c5a53975ee3662a6d
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-24 22:18:53 -0300

    exercises54 dojo jogo da velha

--- 01-fundamentals/05-javascript-dom-events-n-web-storage/04-javascript-web-storage/sd-017-dojo-js-dom/script.js
+++ 01-fundamentals/05-javascript-dom-events-n-web-storage/04-javascript-web-storage/sd-017-dojo-js-dom/script.js
@@ -9,4 +9,29 @@ const combinacoes =[
   [2,4,6] // diagonal direita esquerda
 ]
 
+for(let i  = 0; i < 9; i += 1) {
+  let gridDiv = document.querySelector('.grid');
+  let cellDiv = document.createElement('div');
+  cellDiv.className = 'celula';
+  cellDiv.id = i;
+  gridDiv.appendChild(cellDiv);
+}
 
+let theGrid = document.querySelectorAll('.grid');
+theGrid.addEventListener('click', jogar);
+
+function jogar() {
+  // let pos = "'#" + position + "'";
+  // let target = document.querySelectorAll(pos);
+  // alert(pos);
+  let getPlayer = document.querySelector('#player');
+  let player1 = getPlayer.innerHTML = 'Vez do Jogador X';
+  let player2 = getPlayer.innerHTML = 'Vez do Jogador O';
+  let gridPlay = document.querySelector('.celula');
+  let gridPlayId = gridPlay.id;
+  let gridPlayx = gridPlay.innerHTML = 'x';
+  let gridPlayo = gridPlay.innerHTML = 'o';
+  for (let i = 0; i < 9; i += 1) {
+  alert('click');
+  }
+}
