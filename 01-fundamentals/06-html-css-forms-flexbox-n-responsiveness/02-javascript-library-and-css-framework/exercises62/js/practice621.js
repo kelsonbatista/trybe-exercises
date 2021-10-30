3c4e8134492d549daa172f84f699ba0673929656
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-30 16:31:43 -0300

    practice621 updated

--- 01-fundamentals/06-html-css-forms-flexbox-n-responsiveness/02-javascript-library-and-css-framework/exercises62/js/practice621.js
+++ 01-fundamentals/06-html-css-forms-flexbox-n-responsiveness/02-javascript-library-and-css-framework/exercises62/js/practice621.js
@@ -9,3 +9,73 @@ for (let i = 0; i < state.length; i += 1) {
   stateOption.innerText = state[i].name;
   stateField.appendChild(stateOption);
 }
+
+// DATA PICKER
+document.addEventListener('DOMContentLoaded', function () {
+  let picker = new Pikaday({ 
+    field: document.getElementById('form__start-date'),
+    format: 'DD-MM-YYYY',
+  });
+});
+
+// JUST VALIDATE
+new window.JustValidate('.js-form', {
+  rules: {
+    email: {
+      required: true,
+      email: true,
+      remote: {
+          url: 'https://just-validate-api.herokuapp.com/check-correct',
+          sendParam: 'email',
+          successAnswer: 'OK',
+          method: 'GET'
+      }
+    },
+    name: {
+      required: true,
+      maxLength: 30,
+      minLength: 4,
+    },
+  },
+
+  messages: {
+    email: {
+      remote: 'Email already exist',
+      email: 'Email not valid!'
+    },
+    name: {
+      required: 'Digite seu nome',
+      maxLength: 'Limite excedido!',
+      minLength: 'Nome muito curto!',
+    },
+  },
+
+  focusWrongField: true,
+
+  submitHandler: function (form, values, ajax) {
+      ajax({
+          url: 'https://just-validate-api.herokuapp.com/submit',
+          method: 'POST',
+          data: values,
+          async: true,
+          callback: function (response) {
+              alert('AJAX submit successful! \nResponse from server:' + response)
+          },
+          error: function (response) {
+              alert('AJAX submit error! \nResponse from server:' + response)
+          }
+      });
+  },
+        
+  invalidFormCallback: function (errors) {
+      console.log(errors);
+  },
+});
+
+// BUTTON CLICK
+function formSend(event) {
+  // event.preventDefault();
+}
+
+let btnSend = document.querySelector('#form__send');
+btnSend.addEventListener('click', formSend);
