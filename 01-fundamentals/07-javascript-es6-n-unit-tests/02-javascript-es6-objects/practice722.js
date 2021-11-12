9a33c40e43fe0a739efe250bcdb88ac1d32a03ba
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-11-11 22:47:01 -0300

    Practice 7221

---
+++ 01-fundamentals/07-javascript-es6-n-unit-tests/02-javascript-es6-objects/practice722.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function modifyObject(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+console.log(modifyObject(lesson2, 'turno', 'noite'));
