1a107e646f828b5090455af3ed93b76f830da02b
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-11-15 15:47:45 -0300

    Practice 73-Bonus-1

---
+++ 01-fundamentals/07-javascript-es6-n-unit-tests/03-first-step-with-jest/exercises73/practice73B1.test.js
@@ -0,0 +1,13 @@
+const searchEmployee = require('./practice73B1.js');
+
+describe('Practice 73 Bonus 1', () => {
+
+  it('Verifica se o id não consta no cadastro e retorna o erro "ID não identificada"', () => {
+    expect(searchEmployee('4678-2','firstName')).toThrow('ID não identificada');
+  });
+
+  it('Verifica se a informação não existe e retorna o erro "Informação indisponível"', () => {
+    expect(searchEmployee('9999-9','')).toThrow('Informação indisponível');
+  });
+
+});
