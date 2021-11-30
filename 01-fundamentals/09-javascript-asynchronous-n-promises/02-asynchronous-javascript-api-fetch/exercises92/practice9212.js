9f305f3515ca097c8d64d1c861eba5dbe2af5410
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-11-30 18:21:46 -0300

    Practices 9212

--- 01-fundamentals/09-javascript-asynchronous-n-promises/02-asynchronous-javascript-api-fetch/exercises92/practice9212.js
+++ 01-fundamentals/09-javascript-asynchronous-n-promises/02-asynchronous-javascript-api-fetch/exercises92/practice9212.js
@@ -9,7 +9,17 @@ const fetchJoke = () => {
 
   fetch(API_URL, myObject)
     .then(response => response.json())
-    .then(data => console.log(data));
+    // .then(data => console.log(data));
+    .then(data => {
+      document.getElementById('jokeContainer').innerText = data.joke;
+    });
 };
 
 window.onload = () => fetchJoke();
+
+//   const divJoke = document.getElementById('jokeContainer');
+//   const dataJoke = (JSON.parse(data)).map((e) => {
+//     const textJoke = document.createElement('p');
+//     textJoke.innerHTML = dataJoke.joke;
+//     divJoke.appendChild(textJoke);
+// });
