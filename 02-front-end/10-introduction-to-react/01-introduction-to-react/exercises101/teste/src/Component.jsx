6c89c93c953862868d6b830d57c5da2326a09ab4
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-12-10 19:17:39 -0300

    Exercises 101

---
+++ 02-front-end/10-introduction-to-react/01-introduction-to-react/exercises101/teste/src/Component.jsx
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class Meutexto extends React.Component {
+
+  render() {
+    const element = <div>
+    <h1>Esse é meu título</h1>
+    <p>Essa é uma descrição sobre o elemento.</p>
+    </div>;
+    return element;
+  }
+}
+
+export default Meutexto;
