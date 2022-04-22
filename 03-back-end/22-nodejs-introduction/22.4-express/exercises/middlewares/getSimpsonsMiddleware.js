925e995fb225e76789e9be9435ac63bd43ca4c65
kelsonbatista <kelsonbatista@gmail.com>
2022-04-22 15:59:03 -0300

    Exercise - update

--- 03-back-end/22-nodejs-introduction/22.4-express/exercises/middlewares/getSimpsonsMiddleware.js
+++ 03-back-end/22-nodejs-introduction/22.4-express/exercises/middlewares/getSimpsonsMiddleware.js
@@ -3,9 +3,6 @@ const fsUtils = require('../fs-utils');
 const getSimpsonsMiddleware = async (req, res, _next) => {
   try {
     const simpsons = await fsUtils.readSimpsons();
-    // console.log(req.headers);
-    // const token = req.headers.authorization;
-    // if(!token) return res.status(401).json({ message: "Você não tem autorização para acessar essa página!" });
     return res.status(200).json(simpsons);
   } catch(error) {
     return res.status(500).json({ message: `Error 500: Internal Server Error` })
--- 03-back-end/22-nodejs-introduction/22.4-express/exercises/practice5.js
+++ 03-back-end/22-nodejs-introduction/22.4-express/exercises/practice5.js
@@ -1,21 +1,15 @@
 const express = require('express');
+const mw = require('./middlewares');
 const app = express();
-const port =  3001;
-
-const { 
-  getSimpsonsMiddleware,
-  getSimpsonsByIdMiddleware,
-  postSimpsonsMiddleware,
-  validateTokenMiddleware,
-} = require('./middlewares');
+const port = 3001;
 
 app.use(express.json());
 
-app.get('/simpsons', validateTokenMiddleware, getSimpsonsMiddleware);
+app.get('/simpsons', mw.validateTokenMiddleware, mw.getSimpsonsMiddleware);
 
-app.get('/simpsons/:id', getSimpsonsByIdMiddleware);
+app.get('/simpsons/:id', mw.getSimpsonsByIdMiddleware);
 
-app.post('/simpsons', postSimpsonsMiddleware);
+app.post('/simpsons', mw.postSimpsonsMiddleware);
 
 app.listen(port, () => {
   console.log(`App running at port ${port}`);
