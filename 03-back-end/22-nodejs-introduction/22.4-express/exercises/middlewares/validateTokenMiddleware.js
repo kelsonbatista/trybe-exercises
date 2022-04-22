bb169e333ca36a29232374076cadd891bd284710
kelsonbatista <kelsonbatista@gmail.com>
2022-04-22 15:51:38 -0300

    Exercise - authorization

--- 03-back-end/22-nodejs-introduction/22.4-express/exercises/middlewares/validateTokenMiddleware.js
+++ 03-back-end/22-nodejs-introduction/22.4-express/exercises/middlewares/validateTokenMiddleware.js
@@ -2,6 +2,7 @@ const validateTokenMiddleware = (req, res, next) => {
   const token = req.headers.authorization;
   const tokenLength = token && token.length; // verifica se não existe autorização
   if(!token || tokenLength < 16) return res.status(401).json({ message: "Token inválido!" });
+  console.log("Autorizado!");
   next();
 }
 
