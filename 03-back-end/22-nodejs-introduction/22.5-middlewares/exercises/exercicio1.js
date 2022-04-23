27f02bdee2a3a488e183da2e423a45b806b66ed6
kelsonbatista <kelsonbatista@gmail.com>
2022-04-23 20:45:31 -0300

    Exercise 22.5.1

--- 03-back-end/22-nodejs-introduction/22.5-middlewares/exercises/exercicio1.js
+++ 03-back-end/22-nodejs-introduction/22.5-middlewares/exercises/exercicio1.js
@@ -1,19 +1,46 @@
 const express = require('express');
+const crypto = require('crypto');
+// const rescue = require('express-rescue');
 const port = 3001;
 const app = express();
 
-app.post('/user/register', (req, res) => {
-  const { username, email, password } = req.body;
-  const checkUsername = username.length > 3;
-  const regexEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})$/;
-  const checkEmail = regexEmail.test(email);
-  const regexPassword = /^[0-9]{3,16}$/;
-  const checkPassword = regexPassword.test(password);
+app.use(express.json());
 
-  if(!(checkUsername && checkEmail && checkPassword)) {
-    return res.status(400).json({ message: 'invalid data' });
+const generateToken = (content) => {
+  return crypto.randomBytes(6).toString('hex');
+}
+
+const validateInfo = (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const { email, password } = req.body;
+    const checkUsername = username && username.length > 3;
+    const regexEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})$/;
+    const checkEmail = email && regexEmail.test(email);
+    const regexPassword = /^[0-9]{4,8}$/;
+    const checkPassword = password && regexPassword.test(password);
+
+    const verify = username === undefined
+      ? (!checkUsername && !(checkEmail && checkPassword))
+      : (!(checkUsername && checkEmail && checkPassword));
+
+    if(verify) return res.status(400).json({ message: "invalid data" });
+      //return res.status(200).json({ message: "user created" });
+  } catch(error) {
+      return res.status(500).json({ message: `Error 500 - Internal Server Error - ${error}` });
   }
-  return res.status(200).json({ message: 'user created' });
+  next();
+}
+
+app.post('/user/register', validateInfo);
+
+app.post('/user/login', validateInfo, (_req, res) => {
+    try {
+      const token = generateToken();
+      return res.status(200).json({ token: `${token}` });
+    } catch(error) {
+      return res.status(500).json({ message: "Error 500 - Internal Server Error"})
+    }
 })
 
 app.listen(port, () => console.log(`App running at port ${port}`));
